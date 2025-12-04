ad80ade28e0921af9abb1c837fd951cc2274e779
Shahmeer Athar <shahmeer.athar@gmail.com>
2025-12-04 14:41:59 -0500

    Made day 4 much faster by representing grid as a set

--- 2025/day-four/aoc_day_four.py
+++ 2025/day-four/aoc_day_four.py
@@ -2,45 +2,33 @@ import copy
 
 
 def process_inputs():
-    layout = []
+    layout = set()
     with open("input.txt", "r") as file:
-        for line in file:
-            row = [c for c in line.strip()]
-            layout.append(row)
+        for y, line in enumerate(file):
+            for x, c in enumerate(line.strip()):
+                if c == "@":
+                    layout.add((y, x))
 
     return layout
 
 
-def move_rolls(layout):
+def move_rolls(layout: set):
     rolls_moved = 0
     new_layout = copy.deepcopy(layout)
 
-    for row in layout:
-        print(row)
+    for y, x in layout:
+        num_surrounding_rolls = 0
+        for dy in [-1, 0, 1]:
+            for dx in [-1, 0, 1]:
+                if dy == 0 and dx == 0:
+                    continue
 
-    for y in range(len(layout)):
-        for x in range(len(layout[0])):
-            if layout[y][x] != "@":
-                continue
-            num_surrounding_rolls = 0
-            for dy in [-1, 0, 1]:
-                for dx in [-1, 0, 1]:
-                    adjacent_pos = (y + dy, x + dx)
-                    if (
-                        adjacent_pos[0] < 0
-                        or adjacent_pos[0] >= len(layout)
-                        or adjacent_pos[1] < 0
-                        or adjacent_pos[1] >= len(layout[0])
-                        or (dy == 0 and dx == 0)
-                    ):
-                        continue
+                if (y + dy, x + dx) in layout:
+                    num_surrounding_rolls += 1
 
-                    if layout[adjacent_pos[0]][adjacent_pos[1]] == "@":
-                        num_surrounding_rolls += 1
-
-            if num_surrounding_rolls < 4:
-                new_layout[y][x] = "x"
-                rolls_moved += 1
+        if num_surrounding_rolls < 4:
+            new_layout.remove((y, x))
+            rolls_moved += 1
 
     print("Moved {} rolls".format(rolls_moved))
     return rolls_moved, new_layout
